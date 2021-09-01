a37f3fec29025597c4e09294750517351665e0ae
Jack M. Thompson <WebKruncher@GMail.com>
2021-09-01 18:23:07 +0200

    wip

---
+++ Restful/src/client/main.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) Jack M. Thompson WebKruncher.com, exexml.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the WebKruncher nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Jack M. Thompson ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Jack M. Thompson BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <infokruncher.h>
+#include <infosite.h>
+#include <restful.h>
+
+
+template<> void InfoKruncher::Service< WebKruncher >::ForkAndServe( const SocketProcessOptions& svcoptions )
+{
+	if ( svcoptions.protocol == SocketProcessOptions::Protocol::http )  RunClients< streamingsocket  >( svcoptions );
+	if ( svcoptions.protocol == SocketProcessOptions::Protocol::https ) RunClients< streamingsocket >( svcoptions );
+}
+
+struct Clients : vector< InfoKruncher::Service<WebKruncher> > { void Terminate(); };
+template<> void InfoKruncher::Service< WebKruncher >::Terminate() { subprocesses.Terminate(); }
+void Clients::Terminate() { for ( iterator it=begin(); it!=end(); it++ ) it->Terminate(); }
+
+
+
+int main( int argc, char** argv )
+{
+	stringstream ssexcept;
+	try
+	{
+		cout << green << "Restful is starting up" << normal << endl;
+		InfoKruncher::Options< ClientList > options( argc, argv );
+		if ( ! options ) throw "Invalid options";
+		KruncherTools::Daemonizer daemon( options.daemonize, "RestfulClient" );
+
+		Initialize();
+
+		Clients clients;
+
+		const ClientList& clientlist( options.workerlist );
+		for ( ClientList::const_iterator it=clientlist.begin(); it!=clientlist.end(); it++ )
+		{
+			InfoKruncher::Service<WebKruncher> info;
+			clients.push_back( info );
+			InfoKruncher::Service<WebKruncher>& site( clients.back() );
+			const InfoKruncher::SocketProcessOptions& svcoptions( *it );
+			site.ForkAndServe( svcoptions );
+		}
+		while ( !TERMINATE ) usleep( (rand()%100000)+100000 );
+		clients.Terminate();
+	}
+	catch( const exception& e ) { ssexcept<<e.what(); }
+	catch( const string& s ) { ssexcept<<s;}
+	catch( const char* s ) { ssexcept<<s;}
+	catch( ... ) { ssexcept<<"unknown";}
+	if ( ! ssexcept.str().empty() ) ExceptionLog( "main", ssexcept.str() );
+
+	return 0;
+}
+
---
+++ Restful/src/client/restful.cpp
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) Jack M. Thompson WebKruncher.com, exexml.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the WebKruncher nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Jack M. Thompson ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Jack M. Thompson BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <infokruncher.h>
+#include <infosite.h>
+#include <restful.h>
+
+
+	bool ClientList::operator()( const KruncherTools::Args& options)
+	{
+
+		if ( true )
+		{
+			InfoKruncher::SocketProcessOptions o;
+			o.port=82;
+			o.protocol=InfoKruncher::SocketProcessOptions::Protocol::http;
+			o.path="/home/jmt/websites/text/webkruncher/";
+			push_back( o );
+		}
+
+		KruncherTools::Args::const_iterator usehttps( options.find( "--https" ) );
+		if ( usehttps != options.end() )
+		{
+
+			SecureInformation::init_openssl();
+			InfoKruncher::SocketProcessOptions o;
+			o.port=443;
+			o.protocol=InfoKruncher::SocketProcessOptions::Protocol::https;
+			o.path="/home/jmt/websites/text/webkruncher/";
+
+			const string passwordfile( "/etc/webkruncher.pwd" );
+			if ( KruncherTools::FileExists( passwordfile ) )
+			{
+				o.keypasswd=KruncherTools::LoadFile( passwordfile );
+			} else {
+				cout << "Ssl Password: ";
+				o.keypasswd=KruncherTools::getpass();
+			}
+
+			const string certs( "/home/jmt/websites/certs/webkruncher/" );
+			o.cadir=certs;
+			o.certfile=certs+string("WEBKRUNCHER.COM.crt");
+			o.cafile=certs+string("dv_chain.txt");
+			o.keyfile=certs+string("server.key");
+			push_back( o );
+		}
+		return true;
+	}
+
+	string WebKruncher::LoadResponse( Responder& r  )
+	{
+		int status( 401 );
+
+		stringstream ss;
+
+		const string contenttype(Hyper::ContentType( r.uri ));
+
+		const string filename( r.options.path + r.uri );
+
+		LoadFile(filename.c_str(), ss);
+		if ( ss.str().size() ) status=200;
+
+		const string ExistingCookie( Hyper::mimevalue( r.headers, "cookie" ) );
+		const string CookieName("webkruncher.com");
+
+		string NewCookie;
+
+		if ( ExistingCookie.empty() )
+		{
+			NewCookie=KruncherTools::GetUuid();
+			{stringstream ssl; ssl<<"Created uuid:" << NewCookie; Log( ssl.str() );}
+		}
+
+
+		stringstream response;
+		response << "HTTP/1.1 ";
+		response << status << " " << Hyper::statusText(status) << endl;
+		response << "Content-Type: " << contenttype << endl;
+		response << "Server: WebKruncher" << endl;
+		response << "Connection: close" << endl;
+		response << "Content-Length:" << ss.str().size() << endl;
+		if ( ! NewCookie.empty() ) response << "Set-Cookie:" << CookieName << "=" << NewCookie << ";" << endl;
+		response << endl;
+		response << ss.str();
+
+		string s( response.str() );
+		return s;
+	}
+
+	void WebKruncher::Throttle( const InfoKruncher::SocketProcessOptions& svcoptions )
+	{
+		usleep( (rand()%10)+20 );
+	}
+
+
