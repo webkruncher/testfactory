a7202f7888aea66547f786164550dafdce8c0cbc
Jack M. Thompson <WebKruncher@GMail.com>
2021-09-09 14:34:59 -0400

    ready to print cmake includes

--- utilities/AutoMake2CMake/src/CreateCmakeMacros.cpp
+++ utilities/AutoMake2CMake/src/CreateCmakeMacros.cpp
@@ -4,16 +4,19 @@
 #include <memory>
 #include <vector>
 using namespace std;
-
+#include <infotools.h>
+using namespace KruncherTools;
 
 
 
 struct Lines : vector< string >
 {
+	Lines( const string _where ) : where( _where ) {}
 	virtual ~Lines() {}
 
 	virtual operator bool () = 0;
 	private:
+	const string where;
 	friend ostream& operator<<(ostream&,const Lines&);
 	virtual ostream& operator<<(ostream& o) const = 0;
 }; 
@@ -24,25 +27,102 @@ inline ostream& operator<<(ostream& o,const Lines& m) { return m.operator<<(o); 
 
 struct Macro
 {
-};
+	Macro( const string _path, const string _file ) : path( _path ), file( _file ) {}
+	const string path;
+	const string file;
+	private:
+	friend ostream& operator<<(ostream&,const Macro&);
+	virtual ostream& operator<<(ostream& o) const
+	{
+		o << path << " -->> " << file << endl;
+		return o;
+	} 
+}; 
+
+inline ostream& operator<<(ostream& o,const Macro& m) { return m.operator<<(o); }
+
+
+struct Rules : map< string, stringvector > 
+{
+	Rules( const string _where ) : where( _where ) {}
+	private:
+	const string where;
+	friend ostream& operator<<(ostream&,const Rules&);
+	virtual ostream& operator<<(ostream& o) const
+	{
+		cout << red << where << normal << endl;
+		for (const_iterator it=begin();it!=end();it++) 
+		{
+			o << green << it->first << endl << blue << it->second << normal << endl;
+		}
+		return o;
+	} 
+}; 
+
+inline ostream& operator<<(ostream& o,const Rules& m) { return m.operator<<(o); }
+
 
 struct Macros : vector< Macro >
 {
-};
+	Macros( const string _where ) : rules( _where ) {}
+	void operator+=( const string line )
+	{
+		const string nodots( line.substr( 3, line.size()-3 ) );
+		const size_t ls( nodots.find_last_of( "/" ));
+		if ( ls == string::npos )
+		{
+			cerr << red << nodots << normal << endl;
+		} else {
+			const string path( nodots.substr( 0, ls ) );
+			const size_t ls1( ls + 1 );
+			const string file( nodots.substr( ls1, nodots.size()-ls1 ) );
+			Macro macro( path, file );
+			push_back( macro );
+		}
+		
+	}
+	operator bool()
+	{
+		for (const_iterator it=begin();it!=end();it++) 
+		{
+			const Macro& macro( *it );
+			rules[ macro.path ].push_back( macro.file );
+		}
+		return true;
+	}
+	private:
+	Rules rules;
+	friend ostream& operator<<(ostream&,const Macros&);
+	virtual ostream& operator<<(ostream& o) const
+	{
+		o << rules;
+		return o;
+	} 
+}; 
+
+inline ostream& operator<<(ostream& o,const Macros& m) { return m.operator<<(o); }
 
 struct CompileLines : Lines
 {
+	CompileLines( const string _where ) : Lines( _where ), macros( _where ) {}
 	virtual operator bool ()
 	{
 		for (const_iterator it=begin();it!=end();it++) 
 		{
+			stringvector parts;
+			parts.split( *it, " " );
+			for ( stringvector::const_iterator vit=parts.begin();vit!=parts.end();vit++)
+			{
+				const string part( *vit );
+				const size_t trigger( part.find("../" ) );
+				if ( trigger  == 0 ) macros+=part; }
 		}
 		
-		return true;
+		return !!macros;
 	} 
 	virtual ostream& operator<<(ostream& o) const
 	{
-		for (const_iterator it=begin();it!=end();it++) o << "COMPILE:" << it->c_str() << endl;
+		o << macros;
 		return o;
 	}
 	private:
@@ -51,6 +131,7 @@ struct CompileLines : Lines
 
 struct LinkLines : Lines
 {
+	LinkLines( const string _where ) : Lines( _where ) {}
 	virtual operator bool ()
 	{
 		return false;
@@ -73,8 +154,8 @@ int main( int argc, char** argv)
 
 	unique_ptr< Lines > sublines;
 	
-	if ( how == "-compile" )  sublines.reset( new CompileLines() );
-	if ( how == "-link" )  sublines.reset( new LinkLines() );
+	if ( how == "-compile" )  sublines.reset( new CompileLines( where ) );
+	if ( how == "-link" )  sublines.reset( new LinkLines( where ) );
 
 	Lines& lines( *sublines.get() );
 
