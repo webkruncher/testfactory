feca942a51df4ab50a3862967941f995700d24d0
Jack M. Thompson <WebKruncher@GMail.com>
2021-10-19 16:03:30 +0200

    wip

--- GraphAlgorithms/graph.cpp
+++ GraphAlgorithms/graph.cpp
@@ -6,56 +6,8 @@
 #include <vector>
 #include <map>
 using namespace std;
+#include <graph.h>
 
-struct Edge : pair< int, int >
-{
-	Edge( const int x, const int y ) : pair<int,int>( x,y ) {}
-};
-
-struct EdgeList : vector< Edge >
-{
-};
-
-struct Nodes : vector<int>
-{
-	private:
-	friend ostream& operator<<( ostream&, const Nodes& );
-	ostream& operator<<(ostream& o ) const
-	{
-		for ( const_iterator it=begin();it!=end();it++)
-			o<<(*it)<<" ";
-		return o;
-	}
-};
-inline ostream& operator<<( ostream& o, const Nodes& a) { return a.operator<<(o);}
-
-struct AdjecencyList : map<int, Nodes >
-{
-	void operator<<(const EdgeList& edges)
-	{
-		for ( EdgeList::const_iterator it=edges.begin();it!=edges.end();it++)
-		{
-			const Edge& edge( *it );
-			const int& x( edge.first );
-			const int& y( edge.second );
-			map<int,Nodes>& me( *this );
-			me[x].push_back(y);
-			me[y].push_back(x);
-		}
-	}
-	private:
-	friend ostream& operator<<( ostream&, const AdjecencyList& );
-	ostream& operator<<(ostream& o ) const
-	{
-		for ( const_iterator it=begin();it!=end();it++)
-		{
-			const Nodes& n( it->second );
-			o<<it->first<<":"<<n<<endl;
-		}
-		return o;
-	}
-};
-inline ostream& operator<<( ostream& o, const AdjecencyList& a) { return a.operator<<(o);}
 
 int main(int,char*)
 {
